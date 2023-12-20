805115d68c5d4dddaeba5995b570acdfdc2b136f
0z1oma <akaeluozioma@gmail.com>
2023-12-20 05:36:55 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ * This code prints the alphabets in lowercase
+ */
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+return (0);
+}
