6a7dca160248890ef2d08ff40b93c2560a12018a
0z1oma <akaeluozioma@gmail.com>
2023-12-20 23:17:47 -0800

    3_alph

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ * The code prints the alphabet in lower and uppercases
+ */
+int main(void)
+{
+	char ch = 'a';
+	char bch = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (bch = 'A'; bch <= 'Z'; bch++)
+		putchar(bch);
+	putchar('\n');
+	
+	return (0);
+}
